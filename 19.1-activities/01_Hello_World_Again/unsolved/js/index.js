403c6e8b9d41517230492cf99d40296304c6b5f8
Heather Dadmanesh <hdadmanesh@gmail.com>
2023-01-12 19:12:48 -0500

    19.1

--- 19.1-activities/01_Hello_World_Again/unsolved/js/index.js
+++ 19.1-activities/01_Hello_World_Again/unsolved/js/index.js
@@ -1,3 +1,9 @@
 // Create a variable below
 
+var x="Hello";
+var y= World!";
+var z= x + y;
+var helloWorld='Hello World!';
+// var myFirstVariable = 'variable';
 // Console.log() your variable here.
+console.log(z);
--- 19.1-activities/02_More_Variables/unsolved/js/index.js
+++ 19.1-activities/02_More_Variables/unsolved/js/index.js
@@ -1,3 +1,16 @@
 // Create the addNumbers function here.
+function addNumbers () {
+  
+}
 
 // Create the stringConcat function here.
+
+var x = 1 + 2;
+var y = x + x;
+
+// doSomething();
+
+var newString = 'new' + 'String';
+var blueHat = 'blue' + 'Hat';
+alert('string');
+var tacoCat = 'taco' + 'Cat'
